d0736c03db71660592c8c1b640c4850f869fcefd
NT-Bean <samrahmani2010@gmail.com>
2025-12-01 19:54:42 -0700

    probably gonna break ewsay :smiling_face_with_smiling_eyes_and_three_hearts:

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ```
-                     _
-  __ _ _ __ ___  ___| |__      ____
- / _` | `__/ _ \/ __|  _  \  /      \
-| (_| | | | (_) \__ \ | | |  | >  < |
- \__, |_|  \___/|___/_| |_|  \_.--._/ 
- |___/                       _,)()(,_
+~/grosh $                      _
+~/grosh $   __ _ _ __ ___  ___| |__      ____
+~/grosh $  / _` | `__/ _ \/ __|  _  \  /      \
+~/grosh $ | (_| | | | (_) \__ \ | | |  | >  < |
+~/grosh $  \__, |_|  \___/|___/_| |_|  \_.--._/ 
+~/grosh $  |___/                       _,)()(,_
 ```      
 A collection of custom CLI apps with some very disgusting code.
 
-Note: As of 11/29/2025, I currently have multiple assignments that I should be working on instead of this.
+Note: As of 11/30/2025, I no longer have multiple assignments that I should be working on instead of this! i did them!!
--- src/ewsay.c
+++ src/ewsay.c
@@ -3,26 +3,55 @@
 #include <getopt.h>
 #include <string.h>
 
-char facetop[] = "  ____\n";
-char face2nd[] = "/`    `\\\n";
-char facemid[] = "| >  < |\n";
-char face4th[] ="\\_.--._/\n";
-char facelast[] ="_,)()(,_\n";
+const char facetop[] = "              ____\n";
+const char face2nd[] = "            /`    `\\\n";
+const char facemid[] = "            | >  < |\n";
+const char face4th[] ="            \\_.--._/\n";
+const char facelast[] ="            _,)()(,_\n";
+const char barftop[] = " _,..oo$@)()(}()()(){}(@$oo..,_ \n";
 
+const char barflast[] ="`*$@)(}()(){)()()()(){)()()(@$*`\n";
+
+char* wrapstr(char str[])
+{
+    char input[2134];
+    input[strcspn(input, "\n")] = 0;
+    strcpy(str, input);
+
+    // char strbuff[30] = "\0";
+    for (;;)
+    {
+        for(int i = 30; i >= 0; i--)
+        {
+            if(input[i] == ' ')
+            {
+                // strncpy(strbuff, input, i);
+                strcpy(input + 2, input);
+                strncpy(input, input + 2, 30);
+            }
+        }
+    }
+    
+    return "peepee poopoo";
+}
 int main(int argc, char** argv)
 {
-    printf("%s", facetop);
-    printf("%s", face2nd);
-    printf("%s", facemid);
-    printf("%s", face4th);
-    printf("%s", facelast);
     
-    char testue[50] = "uhoh";
+    char input[2000] = "uh oh except its 30 chars long";
     if (argv[1] != NULL)
     {
-        strcpy(testue, argv[1]);
+        strcpy(input, argv[1]);
     }
-    printf("%s\n", testue);
+
+    if(strlen(input) > 30)
+    {
+
+    }
+
+    printf("%s%s%s%s%s%s", facetop, face2nd, facemid, face4th, facelast, barftop);
+    printf("|%s|", input);
+    
+    printf("\n%s\n", barflast);
 
     return 0;
 }
