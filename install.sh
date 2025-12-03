fb8ec38dadf0e5458bea2aaf34772a4a98d1eedc
NT-Bean <samrahmani2010@gmail.com>
2025-12-03 16:31:34 -0700

    u u uma uma

--- install.sh
+++ install.sh
@@ -5,3 +5,6 @@ sudo /usr/bin/gcc ./src/grossfortune.c -o /usr/local/bin/grossfortune
 
 #ewsay compile
 sudo /usr/bin/gcc ./src/ewsay.c -o /usr/local/bin/ewsay
+
+#caramelldansen compile
+sudo /usr/bin/gcc ./src/caramelldansen.c -o /usr/local/bin/caramelldansen
