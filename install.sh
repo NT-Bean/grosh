327cd3c862d3c523b60a87cd7e307e3a18481037
NT-Bean <samrahmani2010@gmail.com>
2025-12-02 21:49:32 -0700

    i should stop using the a flag

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#grossfortune compilation
+sudo /usr/bin/gcc ./src/grossfortune.c -o /usr/local/bin/grossfortune
+
+#ewsay compile
+sudo /usr/bin/gcc ./src/ewsay.c -o /usr/local/bin/ewsay
